71dbda8c8d9b7d7582561cd2fe271ec6848fee90
GioGio4 <g.ceccobelli@outlook.it>
2023-02-06 14:13:34 +0100

    Create Array users team

--- js/main.js
+++ js/main.js
@@ -27,3 +27,38 @@
 // ----------------------------------------------------------------------------------
 
 // Creiamo una lista(ARRAY) con all'interno tutti i membri del team(OBJECT)  con le loro carratteristiche(PROPRIETA: VALORI)
+
+const users_team = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    photos: "./wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    photos: "./angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    photos: "./walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez	",
+    role: "Social Media Manager",
+    photos: "./angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer	",
+    photos: "./scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    photos: "./barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+console.log(users_team);
